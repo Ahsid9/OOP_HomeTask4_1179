70a209cdf4d92d838f15846c188ba1a850529e96
Ahsid9 <ahsan.siddiqi99@gmail.com>
2025-03-08 17:24:08 +0500

    Add files via upload

---
+++ MealCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class MealCard
+    {
+        public string Student_Name;
+        public int balance;
+
+        public MealCard(string name, int bal=0)
+        {
+            Student_Name = name;
+            if(bal != 0) balance = bal;
+            else balance = 100;
+        }
+        public void AddPoints(int amount)
+        {
+            Console.WriteLine("After Adding Points to Your Balance: {0}",amount + balance);
+        }
+        public void PurchaseFood(int cost)
+        {
+            if (balance > cost) 
+            {
+                balance = balance - cost;
+                Console.WriteLine("Purchased Successful!");
+                Console.WriteLine("Updated Balance: {0}",balance);
+            }
+            else
+            {
+                Console.WriteLine("InSuffecient Balance!");
+                Console.WriteLine("Purchased Unsuccessful!");
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Your Balance is: {0}",balance);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MealCard st1 = new MealCard("Ahsan");
+            st1.Display();
+            st1.PurchaseFood(40);
+            st1.AddPoints(20);
+            Console.WriteLine("");
+            MealCard st2 = new MealCard("Ahsan", 50);
+            st2.Display();
+            st2.PurchaseFood(30);
+            st2.AddPoints(10);
+            Console.WriteLine("");
+            MealCard st3 = new MealCard("Ahsan", 25);
+            st3.Display();
+            st3.PurchaseFood(40);
+            st3.AddPoints(5);
+        }
+    }
+}
